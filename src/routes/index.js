3991232bd1135f85ab9d14724758e0d70804c66d
wzseba <wzseba@gmail.com>
2023-01-15 14:19:09 -0300

    frist commit

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+
+router.get('/get',(req,res)=>{
+    res.json({
+        'name':'sebastian'
+    })
+})
+
+
+module.exports = router;

3ccdcf7a3e8e73f2fae94e019f804ce64b8f8fd4
wzseba <wzseba@gmail.com>
2023-01-15 15:24:14 -0300

    se crea relacion y tabla intermedia

--- src/db.js
+++ src/db.js
@@ -36,10 +36,13 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Dog } = sequelize.models;
+const { Dog, Temperament } = sequelize.models;
 
 // Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+//una raza de perro PERTENECE A MUCHOS temperamentos
+Dog.belongsToMany(Temperament,{through: "dog_temperament"});//genera una tabla intermedia
+//un temperamento PERTENECE A MUCHAS razas de perro
+Temperament.belongsToMany(Dog,{through: "dog_temperament"});//genera una tabla intermedia
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');

a8415e3dd3ff91a7d699b8ac947443e03962cdbb
wzseba <wzseba@gmail.com>
2023-01-15 15:06:33 -0300

    se agregan propiedades al model dog

--- src/models/Dog.js
+++ src/models/Dog.js
@@ -8,5 +8,25 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    weight:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lifeSpan:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image:{
+      type: DataTypes.STRING
+    },
+    createdInDb:{
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
   });
 };

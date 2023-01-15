0287cdc18453b41f23826f23ce638d88a3d44941
wzseba <wzseba@gmail.com>
2023-01-15 15:38:54 -0300

    se agrega primarykey al model dog para que no rompa con el deploy

--- src/models/Dog.js
+++ src/models/Dog.js
@@ -6,7 +6,8 @@ module.exports = (sequelize) => {
   sequelize.define('dog', {
     id:{
       type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
     },
     name: {
       type: DataTypes.STRING,

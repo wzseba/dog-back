5ac8f549162e26c42d48dae28e3f8e2025f0134b
wzseba <wzseba@gmail.com>
2023-01-15 15:14:05 -0300

    se define model temperament con sus propiedades

---
+++ src/models/Temperament.js
@@ -0,0 +1,12 @@
+const { DataTypes, Sequelize } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) =>{
+    //defino modelo
+    sequelize.define('temperament',{
+        name:{
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+}

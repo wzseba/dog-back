1b6c0cd4586a740aaf03f6190bd1af546104675a
wzseba <wzseba@gmail.com>
2023-01-28 00:25:57 -0300

    restaurando lo anterior

--- src/db.js
+++ src/db.js
@@ -7,11 +7,8 @@ const {
 } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
-  logging: false,
-  native: false,
-  host: 'localhost',
-  port: 3001,
-  dialect: 'postgres'
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 
 //PARA PRODUCCION//

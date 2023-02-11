37df6cc22020d6e4da78a26aa10e2dfc7a7a55d6
wzseba <wzseba@gmail.com>
2023-02-11 03:09:56 -0300

    se modifico la function getalldogsdb para formatear temperaments

--- src/controllers/dogController.js
+++ src/controllers/dogController.js
@@ -33,7 +33,29 @@ async function getAllDogsDb(){
         }
     })
 
-    return allDogs
+    const dbFormateo = allDogs.map(dog => {
+        return {
+          id: dog.id,
+          name: dog.name,
+          height: dog.height,
+          weight: dog.weight,
+          temperament: dog.temperaments,
+          life_span: dog.life_span,
+          image: dog.image,
+          createdInDb: dog.createdInDb
+        }
+      })
+    
+      const modificationTemperament = dbFormateo.map(dog => {
+        if(Array.isArray(dog.temperament)) {
+          dog.temperament = dog.temperament.map(t => t.name)
+          dog.temperament = dog.temperament.join(", ")
+        }
+        return dog
+      })
+   
+
+    return modificationTemperament
 }
 
 //funcion que junta lo dogs de la api y los dogs de database
@@ -44,11 +66,15 @@ async function getAllDogs(){
     return allData;
 }
 
-//rutas GET/dogs GET/dogs?name=raza
+/**RUTAS ENDPOINTS */
+
+//rutas GET/dogs ** GET/dogs?name='raza de perro'
 const getDogs = async (req, res, next) =>{
     try {
+
         const {name} = req.query;
         const allDogs = await getAllDogs();
+       
         //busco una raza si se pasa por query sino devuelvo todo el arreglo de la api con los datos necesarios
         if (name) {
             const dogsFilter = allDogs.filter(d => d.name.toLowerCase().trim().includes(name.toLowerCase().trim()));
@@ -62,6 +88,27 @@ const getDogs = async (req, res, next) =>{
         console.log(error);
     }
 }
+
+//ruta GET/dogs?name='raza de perro'
+// const searchDog = async (req,res)=>{
+//     try {
+//         const {name} = req.query;
+//         const allDataDogs = await getAllDogs();//obtiene datos de api como de la base de datos
+
+//         if(name){
+//             const dogFound = allDataDogs.filter(dog => dog.name.toLowerCase().trim().includes(name.toLowerCase().trim()));
+//             dogFound.length ? res.json(dogFound) : res.json({message: "Dog no found in the Data"});
+//         }else{
+//             res.status(400).json({message: "Enter in valid name"})
+//         }
+        
+
+//     } catch (error) {
+//         next(error);
+//         console.log(error);
+//         res.json({message: error})
+//     }
+// }
 
 //rutas GET/dogs/:id
 const getDogById = async (req,res,next)=>{
@@ -82,7 +129,8 @@ const getDogById = async (req,res,next)=>{
 //rutas POST/dogs
 const postDog = async (req,res,next)=>{
     try {
-        const {name, maxheight, minheight, maxweight, minweight, life_span, temperaments, image} = req.body;
+        const {name, maxheight, minheight, maxweight, minweight, life_span, temperament, image} = req.body;
+        
         //validaciones
         if(!name) return res.status(400).json({message: "No se ingreso name"});
         if(!maxheight) return res.status(400).json({message: "No se ingreso altura"});
@@ -90,6 +138,7 @@ const postDog = async (req,res,next)=>{
         if(!life_span) return res.status(400).json({message: "No se ingreso esperanza de vida"});
         if(!image) return res.status(400).json({message: "No se ingreso imagen"});
 
+        // console.log(tempArray);
         const dog = await Dog.create({
             name,
             height: `${minheight} - ${maxheight}`,
@@ -100,32 +149,14 @@ const postDog = async (req,res,next)=>{
 
         const associatedTemperament = await Temperament.findAll({
             where:{
-                name: temperaments
+                name: temperament
             }
         })
-
+      
         //cuando hay asociaciones de models se puede utilizar mixins/metodos
         dog.addTemperament(associatedTemperament);
 
         res.json({message: "Dog created succesfully!"})
-    } catch (error) {
-        next(error);
-        console.log(error);
-        res.json({message: error})
-    }
-}
-
-//ruta GET/dogs?name='raza de perro'
-const searchBreed = async ()=>{
-    try {
-        const {name} = req.query;
-
-        const allDataDogs = await getAllDogs();//obtiene datos de api como de la base de datos
-        
-        const dogFound = allDataDogs.filter(dog => dog.name.toLowerCase().trim().includes(name.toLowerCase().trim()));
-        
-        dogFound.length ? res.json(dogFound) : res.json({message: "Dog no found in the Data"})
-
     } catch (error) {
         next(error);
         console.log(error);
@@ -183,7 +214,6 @@ module.exports = {
     getDogs,
     getDogById,
     postDog,
-    searchBreed,
     deleteDog,
     updateDog
 }

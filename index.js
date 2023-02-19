e6d02190c1937ad5a9803068becf4e3b402d1743
wzseba <wzseba@gmail.com>
2023-02-19 17:40:28 -0300

    force cambio a false para guardar base de datos

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ const { conn } = require('./src/db.js');
 const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+//force: false guarda la base de datos
+//force: true elimina la base de datos
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });

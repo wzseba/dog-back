9450440c8824069151485ddb5678e856753bad03
wzseba <wzseba@gmail.com>
2023-05-06 09:13:54 -0300

    refactor: modificacion de propiedad life_span a lifeSpan para deploy

--- src/models/Dog.js
+++ src/models/Dog.js
@@ -22,7 +22,7 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false
     },
-    life_span:{
+    lifeSpan:{
       type: DataTypes.STRING,
       allowNull: false
     },

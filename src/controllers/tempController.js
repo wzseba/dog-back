60965a3ec9c145c9f6d1742cc7ccdbc28f46ab20
wzseba <wzseba@gmail.com>
2023-08-21 10:57:01 -0300

    fix: se refectorizo codigo para mejor rendimiento

--- src/controllers/tempController.js
+++ src/controllers/tempController.js
@@ -4,32 +4,24 @@ const { PASS_API_KEY } = process.env;
 
 const getTemperaments = async (req,res,next)=>{
     try {
-        //obtengo solo los temperamentos de la api
-        const temperamentsApi = await axios.get(`https://api.thedogapi.com/v1/breeds`);
-        //array de temperamentos
-        const tempData = temperamentsApi.data.map(t => t.temperament);
-        
-        //primero convierto todo el arreglo de en una sola cadena despues convierto a un array seperando cada elemento con split(',')
-        const temps = tempData.toString().split(',');
-        
-        temps.forEach(element => {
-            if(element){
-                let i = element.trim();
-                 //agrego elementos sin espacios vacios al model Temperament
-                Temperament.findOrCreate({
-                    where: { name: i }
-                })
-            }
-        });
-        
-        const tempClean = await Temperament.findAll();
-        
-        res.send(tempClean);
+        const response = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${PASS_API_KEY}`);
+        const temperamentsFromAPI = response.data.map(breed => breed.temperament);
+        const uniqueTemperaments = [...new Set(temperamentsFromAPI.flatMap(temp => temp?.split(',').map(t => t.trim())))];
 
+        await Promise.all(uniqueTemperaments.map(async temperament => {
+            if (temperament) {
+                await Temperament.findOrCreate({
+                    where: { name: temperament }
+                });
+            }
+        }));
+
+        const tempClean = await Temperament.findAll();
+
+        res.send(tempClean);
     } catch (error) {
+        console.error(error);
         next(error);
-        console.log(error);
-        res.json({message: error})
     }
 }
 

29b869d046372c9e5c13b8c4547ebbd2114dfaf9
wzseba <wzseba@gmail.com>
2023-11-29 22:49:40 -0300

    add: probando validaciones

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,16 +1,33 @@
-const {User} = require('../db');//en proceso de desarrollo
+const { User } = require("../db"); //en proceso de desarrollo
 
-const formLogin = async(req,res,next)=>{
+const formLogin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-    try {
-        const {email, password} = req.body;
+    const user = await User.findOne({ where: { email } });
 
-        console.log("Estoy en el controller login" + email + " " + password);
-    } catch (error) {
-        
+    if (!user) {
+      return res.status(401).json({ error: "Invalid email" });
     }
-}
+    console.log('usuario--> ',user);
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Invalid password" });
+    }
+    console.log('password--> ',passwordMatch);
+    const token = jwt.sign({ userId: user.id }, "your-secret-key", {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ token });
+
+  } catch (error) {
+    next(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
 
 module.exports = {
-    formLogin
-}
+  formLogin,
+};

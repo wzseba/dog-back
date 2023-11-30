e6196a9158736348198ee4f798ef84f2e6592475
wzseba <wzseba@gmail.com>
2023-11-29 21:11:02 -0300

    add: nuevo modelo user con campos necesarios para login

---
+++ src/models/User.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require("sequelize");
+const bcrypt = require('bcrypt');
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "user",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      rol: {
+        type: DataTypes.STRING,
+        defaultValue: "user",
+      }
+    },
+    {
+      hooks: {
+        beforeCreate: async (usuario) => {
+          const salt = await bcrypt.genSalt(10);
+          usuario.password = await bcrypt.hash(usuario.password, salt);
+        },
+      },
+    }
+  );
+};

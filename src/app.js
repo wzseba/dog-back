1fa7111364dcd24cd8df81e78d7b4c740301ef21
wzseba <wzseba@gmail.com>
2023-12-03 19:41:13 -0300

    update: se modifico posicion de cookieParse

--- src/app.js
+++ src/app.js
@@ -13,9 +13,9 @@ const server = express();
 
 server.name = 'API';
 
+server.use(cookieParser());
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from

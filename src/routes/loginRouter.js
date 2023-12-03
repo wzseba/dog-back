6d07570650283cc3316eab44122f5ac50a2b9035
wzseba <wzseba@gmail.com>
2023-12-03 19:39:34 -0300

    add: se agrego controller para ruta formLogin

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -1,8 +1,9 @@
 const {Router} = require('express');
 //hacer controller e importar
+const {formLogin} = require('../controllers/loginController.js');
 
 const loginRouter = Router();
 
-loginRouter.get('/login',formLogin);
+loginRouter.post('/',formLogin);
 
 module.exports = loginRouter;

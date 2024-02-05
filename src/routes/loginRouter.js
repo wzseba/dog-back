3cc13a2f92da42a1f3b31cd27eeee9e4e337c51a
wzseba <wzseba@gmail.com>
2024-02-04 22:52:25 -0300

    add: nueva ruta profile

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -1,10 +1,19 @@
-const {Router} = require('express');
-//hacer controller e importar
-const {formLogin, registerUser} = require('../controllers/loginController.js');
+const { Router } = require("express");
+const {
+  formLogin,
+  registerUser,
+  profileUser,
+} = require("../controllers/loginController.js");
+const {
+  validationLogin,
+  validationRegister,
+} = require("../middlewares/validationAuth.js");
+const { accessWithToken } = require("../middlewares/accessToken.js");
 
 const loginRouter = Router();
 
-loginRouter.post('/',formLogin);
-loginRouter.post('/register',registerUser);
+loginRouter.post("/register", validationRegister, registerUser);
+loginRouter.post("/", validationLogin, formLogin);
+loginRouter.get("/profile", accessWithToken, profileUser);
 
 module.exports = loginRouter;

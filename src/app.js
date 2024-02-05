4fc3ed64be9ac7057c213a525f8d83b0a3a74d90
wzseba <wzseba@gmail.com>
2024-02-05 20:38:15 -0300

    refactor: se modifico ruta de sesion de usuarios

--- src/app.js
+++ src/app.js
@@ -1,36 +1,40 @@
-require('dotenv').config();
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const dogRouter = require('./routes/dogRouter.js');
-const tempRouter = require('./routes/tempRouter.js');
-const loginRouter = require('./routes/loginRouter.js');
+require("dotenv").config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const dogRouter = require("./routes/dogRouter.js");
+const tempRouter = require("./routes/tempRouter.js");
+const loginRouter = require("./routes/loginRouter.js");
 
-require('./db.js');
+require("./db.js");
 
 const server = express();
 
-server.name = 'API';
+server.name = "API";
 
 server.use(cookieParser());
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
-server.use(morgan('dev'));
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
+server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
 
-server.use('/login', loginRouter);
-server.use('/dogs', dogRouter);
-server.use('/temperaments', tempRouter);
+server.use("/auth", loginRouter);
+server.use("/dogs", dogRouter);
+server.use("/temperaments", tempRouter);
 
 // Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+server.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);

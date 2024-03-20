e30b1e59954a4dd25fddbbf12c4b5877638c0cf3
wzseba <wzseba@gmail.com>
2024-03-20 20:35:36 -0300

    redeploy

--- src/db.js
+++ src/db.js
@@ -11,8 +11,6 @@ const { DB_USER, DB_PASSWORD, DB_HOST, PORT, DB_DEPLOY } = process.env;
 
 //PARA PRODUCCION nueva
 const sequelize = new Sequelize(DB_DEPLOY, {
-  logging: false,
-  native: false,
   ssl: true,
 });
 

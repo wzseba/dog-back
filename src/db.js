29e09629330a807f75c9c615ef3ef5b326672a11
wzseba <wzseba@gmail.com>
2024-06-23 20:02:30 -0300

    add: new db produccion render

--- src/db.js
+++ src/db.js
@@ -1,15 +1,18 @@
-require("dotenv").config();
-const { Sequelize } = require("sequelize");
-const fs = require("fs");
-const path = require("path");
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, PORT, DB_DEPLOY } = process.env;
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
+//   {
+//     logging: false, // set to console.log to see the raw SQL queries
+//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//   }
+// );
 
-//PARA PRODUCCION nueva
+// PARA PRODUCCION nueva
 const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false,
   native: false,
@@ -21,20 +24,20 @@ const basename = path.basename(__filename);
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, "/models"))
+fs.readdirSync(path.join(__dirname, '/models'))
   .filter(
-    (file) =>
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    file =>
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
   )
-  .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+  .forEach(file => {
+    modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach((model) => model(sequelize));
+modelDefiners.forEach(model => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [
+let capsEntries = entries.map(entry => [
   entry[0][0].toUpperCase() + entry[0].slice(1),
   entry[1],
 ]);
@@ -46,9 +49,9 @@ const { Dog, Temperament, User } = sequelize.models;
 
 // Aca vendrian las relaciones
 //una raza de perro PERTENECE A MUCHOS temperamentos
-Dog.belongsToMany(Temperament, { through: "dog_temperament" }); //genera una tabla intermedia
+Dog.belongsToMany(Temperament, { through: 'dog_temperament' }); //genera una tabla intermedia
 //un temperamento PERTENECE A MUCHAS razas de perro
-Temperament.belongsToMany(Dog, { through: "dog_temperament" }); //genera una tabla intermedia
+Temperament.belongsToMany(Dog, { through: 'dog_temperament' }); //genera una tabla intermedia
 
 Dog.hasMany(User);
 User.belongsTo(Dog);
